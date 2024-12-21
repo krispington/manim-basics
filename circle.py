184dd8caba2d0d57dde91cd0735f22f27b9c7954
aakwsp <88756883+krispington@users.noreply.github.com>
2024-12-21 14:13:00 -0500

    made basic circle
    we made a circle

---
+++ circle.py
@@ -0,0 +1,8 @@
+from manim import *
+
+class test(Scene):
+    #create a circle and animate it
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
